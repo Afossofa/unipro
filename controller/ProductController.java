3647da3f12300fe334e5e00a20b6a1e1239fb923
Afossofa <89802362+Afossofa@users.noreply.github.com>
2024-08-25 14:51:03 +0300

    Add files via upload

---
+++ controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.example.uniproject.controller;
+
+import com.example.uniproject.models.Products;
+import com.example.uniproject.models.Users;
+import com.example.uniproject.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/")
+    public String products(@RequestParam(name = "searchWord", required = false) String title, Principal principal, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
+        model.addAttribute("searchWord", title);
+        return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model, Principal principal) {
+        Products product = productService.getProductById(id);
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
+        model.addAttribute("product", product);
+        model.addAttribute("authorProduct", product.getUser());
+        return "product-info";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct( Products product, Principal principal) {
+        productService.saveProduct(principal, product);
+        return "redirect:/my/products";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id, Principal principal) {
+        productService.deleteProduct(productService.getUserByPrincipal(principal), id);
+        return "redirect:/my/products";
+    }
+
+
+    @GetMapping("/my/products")
+    public String userProducts(Principal principal, Model model) {
+        Users user = productService.getUserByPrincipal(principal);
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "my-products";
+    }
+}
